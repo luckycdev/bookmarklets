6086664f0d8bb074109a310f4fdc4c36ce69d5b3
vincent <50680260+luckycdev@users.noreply.github.com>
2022-11-07 14:08:26 -0600

    Create reddit-search-general.js

---
+++ search/reddit-search-general.js
@@ -0,0 +1,1 @@
+javascript:var a="https://www.reddit.com/search/?q=";var b=prompt("What would you like to search on Reddit?");if (b===""){alert("invalid response");}else if (b!=null){open(a+encodeURI(b));}
