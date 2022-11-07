fbf04cf72203f555df3f8b82893798b38c73d231
vincent <50680260+luckycdev@users.noreply.github.com>
2022-11-07 14:07:04 -0600

    Create stackoverflow-search.js

---
+++ search/stackoverflow-search.js
@@ -0,0 +1,1 @@
+javascript:var a="https://stackoverflow.com/search?q="; var b=prompt("What would you like to search on stackoverflow?", "How to..."); if(b===""){alert("invalid response");}else if (b===" "){alert("invalid response");} else if (b!=null){open(a+encodeURI(b));}
