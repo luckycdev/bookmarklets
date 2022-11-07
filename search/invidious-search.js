f7ac2c2c1dbceea11c91d669c0308c09f1791c0b
vincent <50680260+luckycdev@users.noreply.github.com>
2022-11-07 14:04:22 -0600

    Create invidious-search.js

---
+++ search/invidious-search.js
@@ -0,0 +1,1 @@
+javascript:var a="https://vid.puffyan.us/search?q="; var b=prompt("What would you like to search on youtube(unblocked)?", "MrBeast"); if(b===""){alert("invalid response");}else if (b===" "){alert("invalid response");}else if (b!=null) {open(a+b);}
