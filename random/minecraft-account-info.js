409f6a7e31c6b5ab79e5faa83f6eba6c3850b458
vincent <50680260+luckycdev@users.noreply.github.com>
2022-11-07 14:13:20 -0600

    Create minecraft-account-info.js

---
+++ random/minecraft-account-info.js
@@ -0,0 +1,1 @@
+javascript:var a="https://axis.iaero.me/accinfo?username="; var b=prompt("What is the username of the Minecraft account you would like to look up?", "Notch"); var c="&format=json"; if(b===""){alert("invalid response")}else if(b===" "){alert("invalid response");} else if (b!=null){open(a+b+c);}
