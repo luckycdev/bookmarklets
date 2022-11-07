ddd9e3e4223175c0d61183f69ef52f7e4a2f1b38
vincent <50680260+luckycdev@users.noreply.github.com>
2022-11-07 14:14:06 -0600

    Create tinyurl-shorten.js

---
+++ random/tinyurl-shorten.js
@@ -0,0 +1,1 @@
+javascript:var a="https://tinyurl.com/create.php?url=";var b=location.host;var c=location.pathname+location.search;if (b===""){alert("invalid response");}else if (b===" "){alert("invalid response");}else if (b!=null){open(a+b+c);}
