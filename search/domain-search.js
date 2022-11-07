5395e954f537561d896395787126662ed4d8381f
vincent <50680260+luckycdev@users.noreply.github.com>
2022-11-07 14:10:49 -0600

    Create domain-search.js

---
+++ search/domain-search.js
@@ -0,0 +1,1 @@
+javascript:var a="https://www.name.com/domain/search/";var b=prompt("What domain would you like to lookup?\nPlease do not include http:// or https://", "example.com");if (b===""){alert("invalid response");}else if (b===" "){alert("invalid response");}else if (b!=null){open(a+b);}
