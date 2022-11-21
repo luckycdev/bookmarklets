eacf5bcdd4adcc7e5023fb14a9cf91d1e11825f3
vincent <50680260+luckycdev@users.noreply.github.com>
2022-11-21 11:05:00 -0600

    Create unblock-reddit.js
    unblock reddit with teddit - more instances at https://codeberg.org/teddit/teddit#instances

---
+++ proxy/unblock-reddit.js
@@ -0,0 +1,1 @@
+javascript:var a="https://teddit.net";if(location.hostname==="www.reddit.com"){location.assign(a+location.pathname+location.search);}else{alert("not on reddit");}/*more instances at https://codeberg.org/teddit/teddit#instances*/
