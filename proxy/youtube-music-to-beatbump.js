d85bbf4642d3f52993cf230a49eedb680d328131
vincent <50680260+luckycdev@users.noreply.github.com>
2022-11-22 10:21:56 -0600

    +beatbump, hyperpipe

---
+++ proxy/youtube-music-to-beatbump.js
@@ -0,0 +1,1 @@
+javascript:function yt2bb() {var a="https://bb.vern.cc/";var b="listen?id=";var c=a+b+location.search.split("v=")[1];location.replace(c);}if(location.hostname==="music.youtube.com"){yt2bb();}else{alert("not on youtube music");}/* more instances at https://pastebin.com/raw/YWYPmfQq */
