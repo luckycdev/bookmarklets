85cd321ede05472debe3dee9de814126963ded75
vincent <50680260+luckycdev@users.noreply.github.com>
2022-11-22 18:34:51 -0600

    Update unblock-youtube-music-beatbump.js

--- proxy/unblock-youtube-music-beatbump.js
+++ proxy/unblock-youtube-music-beatbump.js
@@ -1,1 +1,1 @@
-javascript:function yt2bb() {var a="https://bb.vern.cc/";var b="listen?id=";var c=a+b+location.search.split("v=")[1];location.assign(c);}if(location.hostname==="music.youtube.com"){yt2bb();}else{alert("not on youtube music");}/* more instances at https://pastebin.com/raw/YWYPmfQq */
+javascript:function yt2bb() {var a="https://beatbump.ml/";var b="listen?id=";var c=a+b+location.search.split("v=")[1];location.assign(c);}if(location.hostname==="music.youtube.com"){yt2bb();}else{alert("not on youtube music");}/* more instances at https://pastebin.com/raw/YWYPmfQq */
