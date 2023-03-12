bbef5945dfc2b154e45d1f7863fddc4f04395e38
vincent <50680260+luckycdev@users.noreply.github.com>
2023-03-12 15:53:08 -0500

    Update minecraft-account-info.js

--- random/minecraft-account-info.js
+++ random/minecraft-account-info.js
@@ -1,1 +1,1 @@
-javascript:var a="https://axis.iaero.me/accinfo?username="; var b=prompt("What is the username of the Minecraft account you would like to look up?", "Notch"); var c="&format=json"; if(b===""){alert("invalid response")}else if(b===" "){alert("invalid response");} else if (b!=null){open(a+b+c);}
+javascript:var a="https://api.ashcon.app/mojang/v2/user/";var b=prompt("What is the username OR UUID of the Minecraft account you would like to look up?", "Notch");if(b===""){alert("invalid response")}else if(b===" "){alert("invalid response");} else if (b!=null){open(a+b);}
